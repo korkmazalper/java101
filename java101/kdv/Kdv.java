2811b44cabe4db1c8c99eec1d125d6d790354bba
alper.korkmaz <alper.korkmaz@navimatix.de>
2022-10-20 16:26:42 +0200

    Kdv Problemi

---
+++ java101/kdv/Kdv.java
@@ -0,0 +1,13 @@
+package kdv;
+
+import java.util.Scanner;
+
+public class Kdv {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Fiyat: ");
+        int fiyat= scanner.nextInt();
+        System.out.println(fiyat>=1000?fiyat*1.08:fiyat*1.18);
+
+    }
+}
