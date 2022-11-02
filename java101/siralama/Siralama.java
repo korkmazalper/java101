a335148b07ee0d6b39c969c1f4b26be6db4c54a7
alperkorkmaz <dr.alperkorkmaz@gmail.com>
2022-11-02 12:16:12 +0100

    siralama

--- java101/siralama/Siralama.java
+++ java101/siralama/Siralama.java
@@ -12,24 +12,24 @@ public class Siralama {
         b=sc.nextInt();
         System.out.println(" Giriniz c: ");
         c=sc.nextInt();
-        if((a>b)&&(a>c)){
-            if(b>c){
-                System.out.println("a>b>c");
+        if((a<b)&&(a<c)){
+            if(b<c){
+                System.out.println("a<b<c");
             } else{
-                System.out.println("a>c>b");
+                System.out.println("a<c<b");
             }
-        } else if((b>c)&&(b>a)){
-            if(a>c){
-                System.out.println("b>a>c");
+        } else if((b<c)&&(b<a)){
+            if(a<c){
+                System.out.println("b<a<c");
             }
             else {
-                System.out.println("b>c>a");
+                System.out.println("b<c<a");
             }
         } else{
-            if(a>b){
-                System.out.println("c>a>b");
+            if(a<b){
+                System.out.println("c<a<b");
             } else{
-                System.out.println("c>b>a");
+                System.out.println("c<b<a");
             }
         }
     }
