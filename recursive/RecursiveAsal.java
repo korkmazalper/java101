826c6c27b50bcb5c42080416bf6a1e08c6db1e78
alperkorkmaz <dr.alperkorkmaz@gmail.com>
2022-11-23 19:39:52 +0100

    asal sayi recursive fonksiyon

--- recursive/RecursiveAsal.java
+++ recursive/RecursiveAsal.java
@@ -1,6 +1,29 @@
 package recursive;
+
+import java.util.Scanner;
+
 /*kullanicidan alinan bir sayinin asal olup olmadigina
 * recursive bir fonksiyonun sonucu olarak
 * döndüren kod*/
 public class RecursiveAsal {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Pozitif bir tamsayi giriniz: ");
+        int n=sc.nextInt();
+        if (isPrime(n, 2) == true) {
+            System.out.println("Asal");
+        } else {
+            System.out.println("Asal degil");
+        }
+
+    }
+
+    private static boolean isPrime(int n, int i) {
+        if(i==n){
+            return true;
+        } else if(n%i==0){
+            return false;
+        }
+        return isPrime(n,i+1);
+    }
 }
