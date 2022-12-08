4b24ee98e87e6d6b4527734e1d7a718ef9bdb357
alperkorkmaz <dr.alperkorkmaz@gmail.com>
2022-12-08 22:42:59 +0100

    minesweeper

--- minesweeper/MineSweeperPlay.java
+++ minesweeper/MineSweeperPlay.java
@@ -5,11 +5,22 @@ import java.util.Scanner;
 public class MineSweeperPlay {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-        System.out.print("Number of lines: ");
-        int row= scanner.nextInt();
-        System.out.print("Number of columns: ");
-        int column= scanner.nextInt();
-        MineSweeper2 mineSweeper= new MineSweeper2(row,column);
-        mineSweeper.run();
+        boolean keepGame=true;
+        String answer="";
+        while(keepGame){
+            System.out.print("Number of lines: ");
+            int row= scanner.nextInt();
+            System.out.print("Number of columns: ");
+            int column= scanner.nextInt();
+            MineSweeper2 mineSweeper= new MineSweeper2(row,column);
+            mineSweeper.run();
+            System.out.println("One More Game: (Y/N)");
+            answer= scanner.next();
+            if(!("Y".equals(answer.toUpperCase()))){
+                System.out.println("Game over!!!");
+                keepGame=false;
+            }
+        }
+
     }
 }
